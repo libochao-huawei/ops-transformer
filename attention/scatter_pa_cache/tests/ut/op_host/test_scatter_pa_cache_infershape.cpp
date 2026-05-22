d25e7c6da34ef54bc5e21e6a93d06211ff7ef4aa
fishing_artist <liuhuansheng1@h-partners.com>
2026-05-22 15:34:26 +0800

    fix scatter_pa_cache ut
    Co-authored-by: fishing_artist<liuhuansheng1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5748 merge master into master
    
    fix scatter_pa_cache ut
    
    Created-by: fishing_artist
    Commit-by: fishing_artist
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复scatter_pa_cache ut编译和运行问题
    ## 关联的Issue
     [#2594](https://gitcode.com/cann/ops-transformer/issues/2594) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    bash build.sh -u --ophost --ops=scatter_pa_cache
    ![image.png](https://raw.gitcode.com/user-images/assets/7673863/53a1e03b-9ced-4eca-a50c-ea4144c84dd5/image.png 'image.png')
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [X] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!5748

--- attention/scatter_pa_cache/tests/ut/op_host/test_scatter_pa_cache_infershape.cpp
+++ attention/scatter_pa_cache/tests/ut/op_host/test_scatter_pa_cache_infershape.cpp
@@ -10,13 +10,10 @@
 
 #include <gtest/gtest.h>
 #include <iostream>
-#include "infershape_test_util.h"
-#include "kernel_run_context_facker.h"
-#include "log/log.h"
-#include "array_ops.h"
-#include "ut_op_common.h"
-#include "ut_op_util.h"
-#include "../../../op_graph/scatter_pa_cache_proto.h"
+#include "infer_shape_context_faker.h"
+#include "infer_datatype_context_faker.h"
+#include "infer_shape_case_executor.h"
+#include "base/registry/op_impl_space_registry_v2.h"
 
 class ScatterPaCache : public testing::Test {
 protected:
@@ -29,29 +26,25 @@ static void TearDownTestCase() {
 }
 };
 
-TEST_F(ScatterPaCache, template1)
+TEST_F(ScatterPaCache, scatter_pa_cache_infershape_0)
 {
-    const int num_tokens = 102;
-    const int num_head = 1;
-    const int k_head_size = 128;
-    const int num_blocks = 306;
-    const int block_size = 128;
-
-    ge::op::ScatterPaCache op;
-
-    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
-
-    std::vector<int64_t> key_shape = {num_tokens, num_head, k_head_size};
-    std::vector<int64_t> key_cache_shape = {num_blocks, block_size, num_head, k_head_size};
-    std::vector<int64_t> slot_mapping_shape = {num_tokens};
-
-    auto input_key_cache = create_desc_shape_range(key_cache_shape, ge::DT_FLOAT, ge::FORMAT_ND, key_cache_shape, ge::FORMAT_ND, shape_range);
-    op.UpdateInputDesc("key_cache", input_key_cache);
-
-    auto ret = InferShapeTest(op);
-    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
-
-    auto key_cache_shape_cur = op.GetOutputDesc("key_cache");
-    std::vector<int64_t> expected_output_key_cache_shape = key_cache_shape;
-    EXPECT_EQ(key_cache_shape_cur.GetShape().GetDims(), expected_output_key_cache_shape);
+    gert::InfershapeContextPara infershapeContextPara("ScatterPaCache",
+        {
+            {{{102, 1, 128}, {102, 1, 128}}, ge::DT_FLOAT, ge::FORMAT_ND}, // key
+            {{{306, 128, 1, 128}, {306, 128, 1, 128}}, ge::DT_FLOAT, ge::FORMAT_ND}, // keyCache
+            {{{102}, {102}}, ge::DT_INT32, ge::FORMAT_ND}, // slotMapping
+            {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND}, 
+            {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND}, 
+            {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND}, 
+            
+        },
+        {
+            {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // keyCache
+        },
+        {
+            {"cache_mode", Ops::Transformer::AnyValue::CreateFrom<std::string>("Norm")},
+        }
+    );
+    std::vector<std::vector<int64_t>> expectOutputShape = {{306, 128, 1, 128}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
