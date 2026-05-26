845d2659ee649cda578b959199dc93423f8351d6
gcw_2XKemcvz <qinqiuyue@huawei.com>
2026-05-26 11:45:02 +0800

    解决MhcPostBackward处理极值异常
    Co-authored-by: qinqiuyue<qinqiuyue@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5780 merge mhc_5_14 into master
    
    解决MhcPostBackward处理极值异常
    
    Created-by: gcw_2XKemcvz
    Commit-by: qinqiuyue
    Merged-by: cann-robot
    Description: ## 描述
    解决MhcPostBackward处理极值异常
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-transformer/issues/2613
    DTS2026051129667
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!5780

--- mhc/mhc_post_backward/op_kernel/arch35/mhc_post_backward_kernel.h
+++ mhc/mhc_post_backward/op_kernel/arch35/mhc_post_backward_kernel.h
@@ -478,6 +478,7 @@ __aicore__ inline void MhcPostBackwardKernel<T>::ComputeGradHOutTile(LocalTensor
         AscendC::MicroAPI::RegTensor<float> gradOutF32Reg;
         AscendC::MicroAPI::RegTensor<float> hPostLocalReg;
         AscendC::MicroAPI::RegTensor<float> gradHOutTileReg;
+        AscendC::MicroAPI::RegTensor<float> mulResReg;
         AscendC::MicroAPI::RegTensor<T> gradHOutTileB16Reg;
         AscendC::MicroAPI::MaskReg pMask;
         AscendC::MicroAPI::MaskReg pregMain = AscendC::MicroAPI::CreateMask<float,
@@ -489,7 +490,8 @@ __aicore__ inline void MhcPostBackwardKernel<T>::ComputeGradHOutTile(LocalTensor
                 AscendC::MicroAPI::LoadAlign(gradOutF32Reg, gradOutTileAddr + i * actualTileD + k * vfLen);
                 AscendC::MicroAPI::LoadAlign<float, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(hPostLocalReg,
                                                                                                 hPostLocalAddr + i);
-                AscendC::MicroAPI::MulAddDst(gradHOutTileReg, gradOutF32Reg, hPostLocalReg, pMask);
+                AscendC::MicroAPI::Mul(mulResReg, gradOutF32Reg, hPostLocalReg, pMask);
+                AscendC::MicroAPI::Add(gradHOutTileReg, mulResReg, gradHOutTileReg, pMask);
             }
             AscendC::MicroAPI::Cast<T, float, castB32ToB16>(gradHOutTileB16Reg, gradHOutTileReg, pMask);
             AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_PACK_B32>(gradHOutTileAddr + k * vfLen,
@@ -516,6 +518,7 @@ __aicore__ inline void MhcPostBackwardKernel<T>::ComputeGradXTile(LocalTensor<fl
         AscendC::MicroAPI::RegTensor<float> gradOutF32Reg;
         AscendC::MicroAPI::RegTensor<float> hResLocalReg;
         AscendC::MicroAPI::RegTensor<float> gradXTileReg;
+        AscendC::MicroAPI::RegTensor<float> mulResReg;
         AscendC::MicroAPI::RegTensor<T> gradXTileB16Reg;
         AscendC::MicroAPI::MaskReg pMask;
         AscendC::MicroAPI::MaskReg pregMain = AscendC::MicroAPI::CreateMask<float,
@@ -529,7 +532,8 @@ __aicore__ inline void MhcPostBackwardKernel<T>::ComputeGradXTile(LocalTensor<fl
                     AscendC::MicroAPI::LoadAlign(gradOutF32Reg, gradOutTileAddr + j * actualTileD + k * vfLen);
                     AscendC::MicroAPI::LoadAlign<float, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(hResLocalReg,
                                                                                     hResLocalAddr + i * n_ + j);
-                    AscendC::MicroAPI::MulAddDst(gradXTileReg, gradOutF32Reg, hResLocalReg, pMask);
+                    AscendC::MicroAPI::Mul(mulResReg, gradOutF32Reg,  hResLocalReg, pMask);
+                    AscendC::MicroAPI::Add(gradXTileReg, mulResReg, gradXTileReg, pMask);
                 }
                 AscendC::MicroAPI::Cast<T, float, castB32ToB16>(gradXTileB16Reg, gradXTileReg, pMask);
                 AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_PACK_B32>(
