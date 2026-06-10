26d4534d75fffdc49110d780a43d23001328c718
sky-zyh <zhangyuhan45@huawei.com>
2026-06-10 15:31:23 +0800

    opbase commit id update
    Co-authored-by: sky-zyh<zhangyuhan45@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !6685 merge opbase into master
    
    opbase commit id update
    
    Created-by: sky-zyh
    Commit-by: sky-zyh
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [x] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!6685

--- cmake/third_party/opbase.cmake
+++ cmake/third_party/opbase.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPBASE_TAG_ID ee5334d157016d2afa736d4a23381855dbf1fe37)
+set(OPBASE_TAG_ID cd48c600c4b37146f504217eab2bf3956f7fee8b)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../../ops-base")
   get_filename_component(OPBASE_SOURCE_PATH
