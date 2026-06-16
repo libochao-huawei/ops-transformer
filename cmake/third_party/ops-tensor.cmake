7588e372b20f01b6b7ccd610118828117b429c09
luobaiqing <luobaiqing1@huawei.com>
2026-06-16 10:40:55 +0800

    更新 ops-tensor tag：修复 VF 算子 constexpr 接收 CAPI 返回值的兼容问题
    Co-authored-by: luobaiqing<luobaiqing1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !6922 merge update_ops_tensor into master
    
    更新 ops-tensor tag：修复 VF 算子 constexpr 接收 CAPI 返回值的兼容问题
    
    Created-by: luobaiqing
    Commit-by: luobaiqing
    Merged-by: cann-robot
    Description: ## 描述
    
    同步 ops-tensor 仓 tag 至 b24535e(https://gitcode.com/cann/ops-tensor/commit/b24535e30a6f7a3909c53c8b09713a9c0d08e54b?ref=master)，修复 tensor_api 中 VF 算子使用 constexpr 接收 CAPI（asc_get_vf_len）返回值导致的兼容问题。
    
    ops-tensor 对应变更 (cann/ops-tensor!81)：
    - 移除 axpy_vf.h / binary_vf.h / binary_scalar_vf.h / cast_vf.h / dual_vf.h / unary_vf.h 中 `constexpr uint16_t VECTOR_REG_WIDTH = asc_get_vf_len()` 的 constexpr 修饰
    - 涉及 6 个文件，共 +12/-12 行
    
    ## 关联的Issue
    
    无
    
    ## 测试
    
    - [x] ops-transformer CI 编译通过
    
    ## 文档更新
    
    无
    
    ## 类型标签
    
    - [x] ⬆️ 依赖升级
    
    See merge request: cann/ops-transformer!6922

--- cmake/third_party/ops-tensor.cmake
+++ cmake/third_party/ops-tensor.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPTENSOR_TAG_ID af612054d94504dc7d4679a56dbad9fc929e6f13)
+set(OPTENSOR_TAG_ID b24535e30a6f7a3909c53c8b09713a9c0d08e54b)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../ops-tensor")
   get_filename_component(OPTENSOR_SOURCE_PATH
