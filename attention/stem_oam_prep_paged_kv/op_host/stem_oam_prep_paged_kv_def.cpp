6391e79af1a875f6622d3d274ce134807342b139
gh_M <meiguohan@huawei.com>
2026-09-21 11:32:36 +0800

    feat(stem_oam_prep_paged_kv): 支持ascend350平台适配
    Co-authored-by: gh_M<meiguohan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !12187 merge SOPPK_Ascend350 into master
    
    feat(stem_oam_prep_paged_kv): 支持ascend350平台适配
    
    Created-by: gh_M
    Commit-by: gh_M
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    新增DV100-lite芯片，相比DV100，从2D2U变为1D1U，算力、memory规格变为一半（单核的L0A、L0B、L0C、L1等规格不变），NPU ARCH仍与DV100保持一致，SOC VERSION为Ascend 350。需要stem_oam_prep_paged_kv算子新增ascend350平台支持。
    算子代码中，流程控制中放开Ascend350，与Ascend950（原为910_95)拉齐；算子原型增加Ascend350注册
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/5492
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    各泛化测试
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug修复
    - [x] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    See merge request: cann/ops-transformer!12187

--- attention/stem_oam_prep_paged_kv/op_host/stem_oam_prep_paged_kv_def.cpp
+++ attention/stem_oam_prep_paged_kv/op_host/stem_oam_prep_paged_kv_def.cpp
@@ -68,6 +68,7 @@ public:
             .PrecisionReduceFlag(true)
             .ExtendCfgInfo("opFile.value", "stem_oam_prep_paged_kv_apt");
         this->AICore().AddConfig("ascend950", aicore_config);
+        this->AICore().AddConfig("ascend350", aicore_config);
     }
 };
 
